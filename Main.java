6f830a335c784134e2d81f8edbee0c892a8f836c
vince <vincent.tian72@gmail.com>
2018-06-21 15:30:40 +0800

    File added

--- Main.java
+++ Main.java
@@ -1,10 +1,11 @@
 package Challenge;
 
+import java.io.FileNotFoundException;
 import java.math.BigInteger;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		Problem p = new Problem();
 		//System.out.println("Sum is " + p.sumMultiples(1000)); //Problem 1
 		//System.out.println("Sum is " + p.sumFib(50)); //Problem 2
@@ -14,7 +15,11 @@ public class Main {
 		//int[] numbers = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20}; 
 		//System.out.println(p.lcmArray(numbers));
 		//System.out.println(p.sumSquareDifference(new BigInteger("20"))); //Problem 6
-		System.out.println(p.primeNumber(6)); //Problem 7
+		//System.out.println(p.primeNumber(6)); //Problem 7
+		//String filename = "C:\\Users\\vince\\workspace\\ProjectEuler\\Problem8.txt";
+		//System.out.println(p.largestProduct(filename, 13));
+		//System.out.println(p.pythagTriple(1000));
+		System.out.println(p.sumPrime(2000000));
 	}
 
 }
--- Problem.java
+++ Problem.java
@@ -1,8 +1,11 @@
 package Challenge;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Scanner;
 
 
 /**
@@ -12,6 +15,7 @@ import java.util.Collections;
  */
 public class Problem {
 	/**
+	 * Problem 1
 	 * Given a 'number', this method will calculate the sum of all numbers that are multiples of 3 or 5 and
 	 * less than the 'number'.
 	 * @param number Given number
@@ -37,6 +41,7 @@ public class Problem {
 	}
 	
 	/**
+	 * Problem 2
 	 * Given a 'number', method will find the sum of all the even Fibonacci numbers before that 'number'
 	 * @param number Given number
 	 * @return sum
@@ -66,6 +71,7 @@ public class Problem {
 	}
 	
 	/**
+	 * Problem 3
 	 * Method to find the highest prime factor for a specific number
 	 * @param number Given number
 	 * @return highest prime factor of the 'number'
@@ -92,6 +98,7 @@ public class Problem {
 	}
 	
 	/**
+	 * Problem 4
 	 * Method to find the greatest palindrome that is the product of 2, 3 digit numbers
 	 * @return palindrome number
 	 */
@@ -139,6 +146,7 @@ public class Problem {
 	}
 	
 	/**
+	 * Problem 5
 	 * Given an array of numbers, method will return the least common multiple
 	 * @param numbers array
 	 * @return lcm of the numbers in the array
@@ -178,6 +186,7 @@ public class Problem {
 	}
 	
 	/**
+	 * Problem 6
 	 * Method that returns the difference between the square of the sum of numbers and the sum of the squares of numbers
 	 * @param n number n to go up to
 	 * @return the difference
@@ -203,6 +212,7 @@ public class Problem {
 	}
 	
 	/**
+	 * Problem 7
 	 * Initialises an arraylist of prime numbers
 	 */
 	
@@ -220,16 +230,15 @@ public class Problem {
 			if(isPrime(i)){
 				primes.add(i);
 				count++;
-				System.out.println( count + " is less than " + number);
 			}
 		}
 		int index = primes.size() - 1;
 		int maxPrime = primes.get(index);
-		System.out.println(primes);
 		return maxPrime;
 	}
 	
 	/**
+	 * Problem 7
 	 * checks if a number is prime
 	 * @param n number to be checked
 	 * @return true if n is prime, false if n is not prime
@@ -244,4 +253,102 @@ public class Problem {
 		return true;
 	}
 	
+	/**
+	 * Problem 8
+	 * Given a file, return the largest product of consecutive ints that can be formed
+	 * @param filename file to execute code
+	 * @param number of consecutive ints
+	 * @return largest product formed
+	 * @throws FileNotFoundException
+	 */
+	
+	public int largestProduct(String filename, int number) throws FileNotFoundException{
+		int ans = 0;
+		StringBuilder sum = new StringBuilder("");
+		Scanner inFile = new Scanner(new File(filename));
+		while(inFile.hasNext()){
+			StringBuilder value1 = new StringBuilder("" + inFile.nextBigInteger());
+			StringBuilder value2 = new StringBuilder("" + inFile.nextBigInteger());
+			sum = sum.append(value1).append(value2);
+		}	
+	    int length = sum.length();
+	    //String ints = "";
+	    for(int i = 0; i <= length - number; i++){//starting position of each product
+	    	int temp = 1;
+	    	for(int j = i; j < i+number; j++){ //j is used to find product of the number of ints
+	    		temp = Integer.parseInt(sum.substring(j, j+1)) * temp;
+	    	}
+	    	if(temp > ans){
+	    		ans = temp;
+	    		//ints = sum.substring(i, i + number); prints out the consecutive digits
+	    	}
+	    }
+		inFile.close();
+		return ans;
+	}
+	
+	/**
+	 * Problem 9
+	 * Given a number, find a pythagorean triple: a,b,c such that a + b + c = number
+	 * could be improved
+	 * @param number
+	 * @return a*b*c
+	 */
+	public int pythagTriple(int number){
+		int a = 0;
+		int b = 0;
+		int c = 0;
+		for(int i = 1; i < 1000; i++){
+			for(int j = 1; j < 1000; j++){
+				for(int k = 1; k < 1000; k++){
+					if((Math.pow(i, 2) + Math.pow(j, 2) == Math.pow(k, 2)) && (i + j + k == number)){
+						a = i;
+						b = j;
+						c = k;
+					}
+				}
+			}
+		}
+		return a*b*c;
+	}
+	
+	/**
+	 * ArrayList to store prime numbers, used below **bad coding**
+	 */
+	
+	ArrayList<Integer> prime = new ArrayList<>();
+	
+	/**
+	 * Method to find the sum of all prime numbers less than a given 'number'
+	 * @param number given number
+	 * @return sum of all primes below the 'number'
+	 */
+	
+	public int sumPrime(int number){
+		int sum = 0;
+		for(int i = 2; i < number; i++){
+			if(isPrime2(i)){
+				prime.add(i);
+			}
+		}
+		for(int num: prime){
+			sum = sum + num;
+		}
+		return sum;
+	}
+	
+	/**
+	 * Method to check if a number is a prime
+	 * @param n number to be checked
+	 * @return true if prime, else false
+	 */
+	
+	public boolean isPrime2(int n){
+		for(int num: prime){
+			if (n % num == 0){
+				return false;
+			}
+		}
+		return true;
+	}
 }
